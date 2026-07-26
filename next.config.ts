4477c419854de944265accebba75db14b0f1d11f
md nasique <nasiquejahangir000@gmail.com>
2026-07-26 18:35:26 +0530

    Allow external gallery URLs in Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.1.37", "192.168.1.34", "localhost:3000", "localhost:3001", "10.233.157.63"],
+  allowedDevOrigins: [
+    "192.168.1.37",
+    "192.168.1.34",
+    "localhost:3000",
+    "localhost:3001",
+    "10.233.157.63",
+  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
